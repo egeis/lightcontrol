d577ec259a3d29cd3e20850d800be039690c909d
egeis <rcoan@callutheran.edu>
2015-10-01 21:57:52 -0700

    Added Event.

---
+++ src/main/java/com/baedian/lightcontrol/common/PlayerJoinedEvent.java
@@ -0,0 +1,13 @@
+package com.baedian.lightcontrol.common;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+
+public class PlayerJoinedEvent {
+
+	@SubscribeEvent
+	public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event)
+	{
+		
+	}
+}
